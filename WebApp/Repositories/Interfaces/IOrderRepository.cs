40873ad3267398963f7fd3f87e48ad37ef157925
kazenetu <13482856+kazenetu@users.noreply.github.com>
2024-10-20 21:56:07 +0900

    注文リポジトリを追加 #1

---
+++ WebApp/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,16 @@
+using WebApp.Models;
+
+namespace WebApp.Repositories.Interfaces
+{
+  /// <summary>
+  /// 注文用インターフェース
+  /// </summary>
+  public interface IOrderRepository : IRepositoryBase
+  {
+    /// <summary>
+    /// 注文リストを取得
+    /// </summary>
+    /// <returns>注文リスト</returns>
+    List<Order> GetOderList();
+  }
+}

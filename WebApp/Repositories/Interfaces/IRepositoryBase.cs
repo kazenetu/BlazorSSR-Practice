9d0f10b8f2e467bf22cd24b5c4c0bf639e3e0496
kazenetu <13482856+kazenetu@users.noreply.github.com>
2024-10-20 21:54:39 +0900

    リポジトリのスーパークラス追加 #1

---
+++ WebApp/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApp.Repositories.Interfaces
+{
+  /// <summary>
+  /// Repositoryのスーパークラス
+  /// </summary>
+  public interface IRepositoryBase
+  {
+    /// <summary>
+    /// トランザクション設定
+    /// </summary>
+    void BeginTransaction();
+
+    /// <summary>
+    /// コミット
+    /// </summary>
+    void Commit();
+
+    /// <summary>
+    /// ロールバック
+    /// </summary>
+    void Rollback();
+  }
+}

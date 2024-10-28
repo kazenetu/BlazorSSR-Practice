30a8637ed88e6d04b76379f2f4551b82c7c64f4b
kazenetu <13482856+kazenetu@users.noreply.github.com>
2024-10-28 22:13:13 +0900

    セッション名定数を追加 #9

--- WebApp/Models/UserModel.cs
+++ WebApp/Models/UserModel.cs
@@ -10,5 +10,21 @@ namespace WebApp.Models
     /// <param name="AdminRole">管理者権限</param>
     /// <param name="Disabled">無効</param>
     /// <param name="Version">更新バージョン</param>
-    public record UserModel(string ID, string Password, string Salt, string Fullname, bool AdminRole, bool Disabled=true, int Version=1);
+    public record UserModel(string ID, string Password, string Salt, string Fullname, bool AdminRole, bool Disabled=true, int Version=1)
+    {
+        /// <summary>
+        /// セッション名：ユーザー名称
+        /// </summary>
+        public const string SessionFullName = "SessionFullName";
+
+        /// <summary>
+        /// セッション名：管理者権限
+        /// </summary>
+        public const string SessionAdminRole = "SessionAdminRole";
+
+        /// <summary>
+        /// セッション名：ログイン済
+        /// </summary>
+        public const string SessionLoggedIn = "SessionLoggedIn";
+    }
 }

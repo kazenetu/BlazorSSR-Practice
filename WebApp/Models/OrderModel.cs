64d8bfa7b2b4187b3f7c72f4004c2e2e2d5f485d
kazenetu <13482856+kazenetu@users.noreply.github.com>
2024-11-13 22:48:57 +0900

    編集画面で利用するため、注文モデルをrecordからclassに変更 #21

--- WebApp/Models/OrderModel.cs
+++ WebApp/Models/OrderModel.cs
@@ -1,12 +1,47 @@
 namespace WebApp.Models
 {
-    /// <summary>
-    /// 注文情報
-    /// </summary>
-    /// <param name="No">番号(1〜)</param>
-    /// <param name="ProductName">商品名</param>
-    /// <param name="UnitPrice">単価</param>
-    /// <param name="Qty">数量</param>
-    /// <param name="TotalPrice">合計金額</param>
-    public record OrderModel(int No, string ProductName, decimal UnitPrice, decimal Qty, decimal TotalPrice);
+    public class OrderModel
+    {
+        /// <summary>
+        /// 番号(1〜)
+        /// </summary>
+        public int No {set;get;}
+
+        /// <summary>
+        /// 商品名
+        /// </summary>
+        public string ProductName{set;get;}
+
+        /// <summary>
+        /// 単価
+        /// </summary>
+        public decimal UnitPrice{set;get;}
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public decimal Qty{set;get;}
+
+        /// <summary>
+        /// 合計金額
+        /// </summary>
+        public decimal TotalPrice{set;get;}
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="no">番号(1〜)</param>
+        /// <param name="productName">商品名</param>
+        /// <param name="unitPrice">単価</param>
+        /// <param name="qty">数量</param>
+        /// <param name="totalPrice">合計金額</param>
+        public OrderModel(int no, string productName, decimal unitPrice, decimal qty, decimal totalPrice)
+        {
+            No = no;
+            ProductName = productName;
+            UnitPrice = unitPrice;
+            Qty = qty;
+            TotalPrice = totalPrice;
+        }
+    }
 }

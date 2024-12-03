42d9db86c7299198c5a1356607eafe1b7e273735
kazenetu <13482856+kazenetu@users.noreply.github.com>
2024-12-03 16:59:56 +0900

    ユーザー用インターフェースのスコープを削除 #61

--- WebApp/Repositories/Interfaces/IUserRepository.cs
+++ WebApp/Repositories/Interfaces/IUserRepository.cs
@@ -12,7 +12,7 @@ namespace WebApp.Repositories.Interfaces
         /// </summary>
         /// <param name="userID">ユーザーID</param>
         /// <returns>ユーザー</returns>
-        public UserModel? GetUser(string userID);
+        UserModel? GetUser(string userID);
 
         /// <summary>
         /// ユーザーの保存
@@ -30,13 +30,13 @@ namespace WebApp.Repositories.Interfaces
         /// <param name="userID">ユーザーID</param>
         /// <param name="password">パスワード</param>
         /// <returns>パスワード一致か否か</returns>
-        public bool EqalsPassword(string userID, string password);
+        bool EqalsPassword(string userID, string password);
 
         /// <summary>
         /// パスワード作成
         /// </summary>
         /// <param name="password">パスワード</param>
         /// <returns>暗号化済パスワードとソルトのタプル</returns>
-        public (string encryptedPassword, string salt) CreateEncryptedPassword(string password);
+        (string encryptedPassword, string salt) CreateEncryptedPassword(string password);
     }
 }

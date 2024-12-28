171cd6d6c183989be37fe6a0add05e4234856b03
kazenetu <13482856+kazenetu@users.noreply.github.com>
2024-12-28 18:01:25 +0900

    Repositoryの「無条件の全件検索メソッド」を削除 #79

--- WebApp/Repositories/Interfaces/IWeatherForecastRepository.cs
+++ WebApp/Repositories/Interfaces/IWeatherForecastRepository.cs
@@ -7,21 +7,6 @@ public interface IWeatherForecastRepository : IRepositoryBase
     /// <summary>
     /// レコード数を返す
     /// </summary>
-    /// <returns>対象レコード数</returns>
-    int GetTotalRecord();
-
-    /// <summary>
-    /// 対象レコード配列を返す
-    /// </summary>
-    /// <param name="inputModel">検索条件</param>
-    /// <param name="startIndex">取得開始レコードインデックス</param>
-    /// <param name="recordCount">取得レコード数</param>
-    /// <returns>指定範囲のレコード配列</returns>
-    WeatherForecastModel[] GetList(int startIndex, int recordCount);
-
-    /// <summary>
-    /// レコード数を返す
-    /// </summary>
     /// <param name="inputModel">検索条件</param>
     /// <returns>対象レコード数</returns>
     int GetTotalRecord(InputWeatherForecastModel inputModel);
--- WebApp/Repositories/WeatherForecastRepository.cs
+++ WebApp/Repositories/WeatherForecastRepository.cs
@@ -20,62 +20,6 @@ public class WeatherForecastRepository : RepositoryBase, IWeatherForecastReposit
     /// <summary>
     /// レコード数を返す
     /// </summary>
-    /// <returns>対象レコード数</returns>
-    public int GetTotalRecord()
-    {
-        var result = 0;
-
-        // パラメータ初期化
-        db!.ClearParam();
-
-        var sql = new StringBuilder();
-
-        sql.AppendLine("SELECT count(target_date) AS cnt FROM t_teather_forecast");
-
-        var sqlResult = db.Fill(sql.ToString());
-        foreach (DataRow row in sqlResult.Rows)
-        {
-            result = Parse<int>(row["cnt"]);
-            break;
-        }
-
-        return result;
-    }
-
-    /// <summary>
-    /// 対象レコード配列を返す
-    /// </summary>
-    /// <param name="startIndex">取得開始レコードインデックス</param>
-    /// <param name="recordCount">取得レコード数</param>
-    /// <returns>指定範囲のレコード配列</returns>
-    public WeatherForecastModel[] GetList(int startIndex, int recordCount)
-    {
-        var result = new List<WeatherForecastModel>();
-
-        // パラメータ初期化
-        db!.ClearParam();
-
-        var sql = new StringBuilder();
-
-        sql.AppendLine("SELECT target_date, temperature_c, summary FROM t_teather_forecast");
-        sql.AppendLine("ORDER BY target_date");
-        sql.AppendLine(db!.GetLimitSQL(recordCount, startIndex));
-
-        var sqlResult = db.Fill(sql.ToString());
-        foreach (DataRow row in sqlResult.Rows)
-        {
-            // 天気予報情報
-            var targetDate = Parse<string>(row["target_date"]);
-            var temperatureC = Parse<int>(row["temperature_c"]);
-            var summary = Parse<string>(row["summary"]);
-            result.Add(new WeatherForecastModel(DateOnly.Parse(targetDate), temperatureC, summary));
-        }
-        return [.. result];
-    }
-
-    /// <summary>
-    /// レコード数を返す
-    /// </summary>
     /// <param name="inputModel">検索条件</param>
     /// <returns>対象レコード数</returns>
     public int GetTotalRecord(InputWeatherForecastModel inputModel)

9162ba6f21d9307ea1a4ac17fa3e0242114d376b
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-01-19 18:10:54 +0900

    ダウンロード用モデル の「ファイル名」が指定なしの場合は例外エラーとする実装を追加 #93

--- WebApp/Models/Parts/DownLoadModel.cs
+++ WebApp/Models/Parts/DownLoadModel.cs
@@ -3,7 +3,42 @@ namespace WebApp.Models.Parts;
 /// <summary>
 /// ダウンロード用モデル
 /// </summary>
-/// <param name="FileName">ファイル名</param>
-/// <param name="Data">データ</param>
-/// <param name="ErrorMessage">エラーメッセージ</param>
-public record DownLoadModel(string FileName, object? Data, string ErrorMessage = "");
+public class DownLoadModel
+{
+    /// <summary>
+    /// ダウンロード用モデル
+    /// </summary>
+    /// <param name="fileName">ファイル名</param>
+    /// <param name="data">データ</param>
+    /// <param name="errorMessage">エラーメッセージ</param>
+    public DownLoadModel(string fileName, object? data, string errorMessage = "")
+    {
+        FileName = fileName;
+        Data = data;
+        ErrorMessage = errorMessage;
+
+        // エラーメッセージがない場合のみチェック
+        if (string.IsNullOrEmpty(errorMessage))
+        {
+            // ファイル名がない場合は例外エラー
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            if (string.IsNullOrEmpty(fileNameWithoutExtension))
+                    throw new Exception("ファイル名が指定されていません。");
+            }
+        }
+
+    /// <summary>
+    /// ファイル名
+    /// </summary>
+    public string FileName { get; init; }
+
+    /// <summary>
+    /// データ
+    /// </summary>
+    public object? Data { get; init; }
+
+    /// <summary>
+    /// エラーメッセージ
+    /// </summary>
+    public string ErrorMessage { get; init; }
+}

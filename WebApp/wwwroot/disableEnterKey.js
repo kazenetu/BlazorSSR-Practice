9375e6eb7ed550686f5e5430ed0397d95f9f4373
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-02-16 15:49:57 +0900

    「suppressEnterKey」を「disableEnterKey」に改名 #117

--- WebApp/wwwroot/disableEnterKey.js
+++ WebApp/wwwroot/disableEnterKey.js
@@ -1,10 +1,10 @@
-window.suppressEnterKey = async () => {
+window.disableEnterKey = async () => {
     const items = document.getElementsByTagName('input');
     for (var index in items) {
         // メソッドの場合は処理なし
         if (isNaN(parseInt(index))) continue;
 
-        // inoutタグにエンターキー無効化を実施
+        // inputタグにエンターキー無効化を実施
         items[index].addEventListener('keydown', function (event) {
             if (event.key === 'Enter') {
                 event.preventDefault();

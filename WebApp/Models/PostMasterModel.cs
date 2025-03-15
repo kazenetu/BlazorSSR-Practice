66008276781e5ddd75784e14f01c9c1910f06042
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-03-15 13:13:52 +0900

    PostMasterModelの一部プロパティの名付けミスを修正 #137

--- WebApp/Models/PostMasterModel.cs
+++ WebApp/Models/PostMasterModel.cs
@@ -6,8 +6,8 @@ namespace WebApp.Models;
 /// <param name="PostCd">郵便番号</param>
 /// <param name="Todohuken">都道府県名</param>
 /// <param name="Shikuson">市町村名</param>
-/// <param name="machi">町名</param>
+/// <param name="Machi">町名</param>
 /// <param name="TodohukenKana">都道府県名カナ</param>
 /// <param name="ShikusonKana">市町村名カナ</param>
-/// <param name="machiKana">町名カナ</param>
-public record PostMasterModel(string PostCd, string Todohuken, string Shikuson, string machi, string TodohukenKana = "", string ShikusonKana = "", string machiKana = "");
+/// <param name="MachiKana">町名カナ</param>
+public record PostMasterModel(string PostCd, string Todohuken, string Shikuson, string Machi, string TodohukenKana = "", string ShikusonKana = "", string MachiKana = "");

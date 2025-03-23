880ef7fe915c873daf0200c19e715a60a5c178b3
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-03-23 23:14:10 +0900

    PageBase.jsに指定文字列のフリガナ取得メソッドを追加 #143

--- WebApp/Components/Pages/Base/PageBase.razor.js
+++ WebApp/Components/Pages/Base/PageBase.razor.js
@@ -19,3 +19,26 @@ window.kanjiToFurigana = (kanjiID, furiganaID) => {
 
     furigana.value = result;
 };
+
+window.getFurigana = (kanji) => {
+    let result = '';
+    const items = tokenizerInstance.tokenize(kanji);
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+
+        if (item.pos_detail_1 === "数") {
+            result += item.basic_form;
+            continue;
+        }
+
+        if (item.pos === "記号") {
+            result += item.basic_form;
+            continue;
+        }
+
+        if (item.reading == undefined) result += item.surface_form;
+        else result += item.reading;
+    }
+
+    return result;
+};

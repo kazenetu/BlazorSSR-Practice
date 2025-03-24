88a6afdf8df37743254439269cad71136a77b3e0
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-03-24 20:52:34 +0900

    PageBase.jsのフリガナ取得系メソッドの処理統一 #143

--- WebApp/Components/Pages/Base/PageBase.razor.js
+++ WebApp/Components/Pages/Base/PageBase.razor.js
@@ -10,24 +10,22 @@ window.kanjiToFurigana = (kanjiID, furiganaID) => {
     const kanji = document.getElementById(kanjiID);
     const furigana = document.getElementById(furiganaID);
 
-    let result = '';
-    let items = tokenizerInstance.tokenize(kanji.value);
-    for(let i=0;i<items.length;i++){
-        if(items[i].reading == undefined) result += items[i].surface_form;
-        else result += items[i].reading;
-    }
-
-    furigana.value = result;
+    furigana.value = getKana(kanji.value);
 };
 
 window.getFurigana = (kanji) => {
+    return getKana(kanji);
+};
+
+window.getKana = (kanji) => {
     let result = '';
     const items = tokenizerInstance.tokenize(kanji);
     for (let i = 0; i < items.length; i++) {
         const item = items[i];
 
         if (item.pos_detail_1 === "数") {
-            result += item.basic_form;
+            if (item.basic_form === '*') result += item.surface_form;
+            else result += item.basic_form;
             continue;
         }
 

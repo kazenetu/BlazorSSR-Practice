7ae2b70315676d58cb506e9dbc9e19f35eb7ac38
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-12-15 23:03:50 +0900

    Program.csの仮実装 #244

--- Tools/CreateMaster/Program.cs
+++ Tools/CreateMaster/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 引数チェック
+using System.Data;
+using DB;
+
+if (args.Length < 3)
+{
+    Console.WriteLine("dotnet run DBモード 接続文字列 テーブル名");
+    Console.WriteLine("　DBモード：SQLite/DB/PostgreSQL");
+    return;
+}
+
+// DB取得
+var dbMode = args[0];
+IDB db = dbMode switch
+{
+    "SQLite" => new SQLiteDB(),
+    "PostgreSQL" => new PostgeSQLDB(),
+    _ => throw new ArgumentOutOfRangeException(nameof(dbMode), $"Not expected direction value: {dbMode}"),
+};
+
+// スキーマ取得
+var schemaTable = db.GetSchema(args[1], args[2]);
+
+// HACK スケルトンコード作成
+foreach(DataColumn col in schemaTable.Columns)
+{
+    Console.WriteLine($"{col.DataType} {col} \tnotnull:{col.AllowDBNull} \tdef:{col.DefaultValue}");
+}

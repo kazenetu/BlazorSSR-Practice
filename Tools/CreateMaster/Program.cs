c1e9c46cdd35cf3ad32fadc77ef3c659552fb41f
kazenetu <13482856+kazenetu@users.noreply.github.com>
2025-12-29 16:31:13 +0900

    パラメータオプションの指定方法を指定方式に変更 #244

--- Tools/CreateMaster/Program.cs
+++ Tools/CreateMaster/Program.cs
@@ -2,8 +2,13 @@
 using System.Data;
 using DB;
 using Create;
+using Common;
 
-if (args.Length < 3)
+// パラメータ取得
+var argManager = new ArgManagers(args);
+
+// ヘルプモードの確認
+if (argManager.GetRequiredArgCount() < 3)
 {
     Console.WriteLine("dotnet run DBモード 接続文字列 テーブル名 [ベースURL] [ハイフンモード]");
     Console.WriteLine("　DBモード：SQLite/DB/PostgreSQL");
@@ -13,7 +18,7 @@ if (args.Length < 3)
 }
 
 // DB取得
-var dbMode = args[0];
+var dbMode = argManager.GetRequiredArg(0);
 IDB db = dbMode switch
 {
     "SQLite" => new SQLiteDB(),
@@ -22,24 +27,23 @@ IDB db = dbMode switch
 };
 
 // スキーマ取得
-var schemaTable = db.GetSchema(args[1], args[2]);
+var connectionString = argManager.GetRequiredArg(1);
+var dbTableName = argManager.GetRequiredArg(2);
+var schemaTable = db.GetSchema(connectionString!, dbTableName!);
 
-//　オプション：ベースURL
-string? urlPrefix = null;
-if (args.Length >= 4)
-{
-    urlPrefix = args[3];
-}
+//　オプション：プレフィックスURL
+var urlPrefix = argManager.GetOptionArg(new List<string>() { "--url_prefix", "-u" });
 
-//　オプション：ベースURL
-bool urlUseHyphen = true;
-if (args.Length >= 5 && bool.TryParse(args[4].ToLower(), out var useHyphen))
+//　オプション：ハイフンモード
+var urlUseHyphen = true;
+var  useHyphen = argManager.GetOptionArg(new List<string>() { "--use_hyphen", "-useHyp" });
+if (!string.IsNullOrEmpty(useHyphen) && useHyphen.ToLower() == "false")
 {
-    urlUseHyphen = useHyphen;
+    urlUseHyphen = false;
 }
 
 // スケルトンコード作成
 var outputPath = Environment.CurrentDirectory+"/Output";
-var createFiles = new CreateFils(outputPath, schemaTable.Columns, args[2], urlPrefix, urlUseHyphen);
+var createFiles = new CreateFils(outputPath, schemaTable.Columns, dbTableName!, urlPrefix, urlUseHyphen);
 createFiles.Create();
 

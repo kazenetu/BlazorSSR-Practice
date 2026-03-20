2627e5bb070b3a5a0164a20578ef157c77e2f0cb
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-03-20 20:40:43 +0900

    Model：コンストラクタメソッド：有効フラグパラメータの省略値をtrueに変更 #256

--- WebApp/Models/OrderModel.cs
+++ WebApp/Models/OrderModel.cs
@@ -46,7 +46,7 @@ public class OrderModel
     /// <param name="qty">数量</param>
     /// <param name="totalPrice">合計金額</param>
     /// <param name="version">バージョン番号(楽観ロック用)</param>
-    public OrderModel(int no, string productName, decimal unitPrice, decimal qty, decimal totalPrice, bool enabled = false, int version = 0)
+    public OrderModel(int no, string productName, decimal unitPrice, decimal qty, decimal totalPrice, bool enabled = true, int version = 0)
     {
         No = no;
         ProductName = productName;
